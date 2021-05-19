d875b2aac664e8abcf2c0f6099fbd815ee67b4d8
RelaperCrystal <RelaperCrystal@163.com>
2021-05-19 11:30:26 +0800

    :recycle: Add .editorconfig

--- CityCompanion/AmmuNation.cs
+++ CityCompanion/AmmuNation.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using CityCompanion.Util;
 using GTA;
 using GTA.Math;

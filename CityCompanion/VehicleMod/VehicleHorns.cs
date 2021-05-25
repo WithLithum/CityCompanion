250948d517dd237704d72da6cf9beaa4fb4ba628
RelaperCrystal <RelaperCrystal@163.com>
2021-05-25 10:07:10 +0800

    :construction: Declare vehicle horn types (#1)

---
+++ CityCompanion/VehicleMod/VehicleHorns.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) RelaperCrystal.
+// Licensed under GNU GPL version 3 or any later version.
+
+namespace CityCompanion.VehicleMod
+{
+    public enum VehicleModHorns
+    {
+        Stock = -1,
+        Truck,
+        Police,
+        Clown,
+        Musical1,
+        Musical2,
+        Musical3,
+        Musical4,
+        Musical5,
+        SadTrombone,
+        Classical1,
+        Classical2,
+        Classical3,
+        Classical4,
+        Classical5,
+        Classical6,
+        Classical7,
+        ScaleDo,
+        ScaleRe,
+        ScaleMi,
+        ScaleFa,
+        ScaleSo,
+        ScaleLa,
+        ScaleTi,
+        ScaleDoHigh,
+        Jazz1,
+        Jazz2,
+        Jazz3,
+        JazzLoop,
+        StarsPangBan1,
+        StarsPangBan2,
+        StarsPangBan3,
+        StarsPangBan4,
+        ClassicalLoop,
+        Classical8,
+        ClassicalLoop2
+    }
+}

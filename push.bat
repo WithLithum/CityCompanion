84ef7be5986afb2af834055de0c101cd96f293bc
RelaperCrystal <RelaperCrystal@163.com>
2021-06-10 20:02:03 +0800

    :memo: Optimize push scripts for each platform

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
-# pushing information :)
-# push to our main repository
+@echo off
+echo Pushing to remote (main), please wait.... (1/2)
 git push
+echo Pushing to remote (mirror), please wait.... (2/2)
 git push mirror
-
-# neat
+@echo on
--- push.ps1
+++ push.ps1
@@ -1,6 +1,6 @@
-# pushing information :)
-# push to our main repository
+Write-Host "=-=-=-=-=-= GITHUB & BITBUCKET PUSH SCRIPT =-=-=-=-=-=" -ForegroundColor White
+Write-Host "Pushing to remote Bitbucket, please wait (1/2)" -ForegroundColor DarkGreen
 git push
+Write-Host "Pushing to remote GitHub, please wait (2/2)" -ForegroundColor DarkGreen
 git push mirror
-
-# neat
+Write-Host "PUSH SUCCESSFUL (2/2)" -ForegroundColor Green
